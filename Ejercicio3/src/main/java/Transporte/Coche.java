a20b4479f356069c956d23abd21d9bda59e43ce6
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-03-23 13:08:57 -0500

    Ejercicio 3

---
+++ Ejercicio3/src/main/java/Transporte/Coche.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Transporte;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Coche extends Transporte{
+
+    @Override
+    public void moverse() {
+        System.out.println("El choche se esta moviendo");
+    }
+    
+}
---
+++ Ejercicio3/src/main/java/Transporte/Ejercicio3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Transporte;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        Transporte bicicleta = new Bicicleta();
+        Transporte coche = new Coche();
+        
+        bicicleta.moverse();
+        coche.moverse();
+    }
+}
